c870013933e68d3ef014699803cc7ca007a14ba0
Gonza <peraltag@gmail.com>
2016-08-21 23:42:48 -0300

    Arreglo fix de version npm y node

--- angular2-quickstart/app/app.component.js
+++ angular2-quickstart/app/app.component.js
@@ -20,7 +20,7 @@ var AppComponent = (function () {
         this.title = 'Tour of Heroes';
         this.hero = {
             id: 1,
-            name: 'Windstorme'
+            name: 'Windstorm'
         };
     }
     AppComponent = __decorate([
@@ -33,4 +33,16 @@ var AppComponent = (function () {
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var HEROES = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
 //# sourceMappingURL=app.component.js.map
