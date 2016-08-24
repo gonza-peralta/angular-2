4c30702e312bc220614eb26e0c91abddc147714e
Gonza <peraltag@gmail.com>
2016-08-24 00:39:24 -0300

    Add a service interface

--- angular2-quickstart/app/app.component.ts
+++ angular2-quickstart/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
 
 
 @Component({
+	providers: [HeroService],
   selector: 'my-app',
   styles: [`
   .selected {
@@ -65,25 +68,18 @@ import { HeroDetailComponent } from './hero-detail.component';
 	<my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
   selectedHero: Hero;
-	heroes = HEROES;
+	heroes: Hero[];
+	constructor(private heroService: HeroService) { };
 	onSelect(hero: Hero): void {
   	this.selectedHero = hero;
 	}
-
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  ngOnInit(): void {
+  	this.getHeroes();
+  }
 }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
