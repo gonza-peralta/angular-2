b742be93724119bf1c3c90040a450d052b7ee09a
Gonza <peraltag@gmail.com>
2016-08-29 00:33:54 -0300

    Limpio el repo sacando js y js.map

--- angular2-quickstart/app/app.module.ts
+++ angular2-quickstart/app/app.module.ts
@@ -1,17 +1,41 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { routing } 			 from './app.routing';
+
+// Imports for loading & configuring the in-memory web api
+//import { XHRBackend } from '@angular/http';
+
+//import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+//import { InMemoryDataService }               from './in-memory-data.service';
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
+
 import { AppComponent }  from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService,
   ],
   bootstrap: [ AppComponent ]
 })
