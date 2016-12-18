69101279f7e5ebea5d5363c131f431dbabd4827c
Gonza <peraltag@gmail.com>
2016-12-18 17:46:50 -0300

    Borro y arranco de nuevo

--- angular2-quickstart/app/main.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app.module';
-// this is a comment
-platformBrowserDynamic().bootstrapModule(AppModule);
