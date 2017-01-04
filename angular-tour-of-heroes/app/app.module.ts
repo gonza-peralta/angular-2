af8b0e2c4ad12d9389a387a9f64148cf9fd23976
Gonza <peraltag@gmail.com>
2017-01-04 00:20:44 -0300

    Agrego router y mas componentes

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -1,18 +1,51 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService }         from './hero.service';
+
+import { RouterModule }   from '@angular/router';
+
+
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+		  {
+		    path: 'heroes',
+		    component: HeroesComponent
+		  },
+		  {
+			  path: 'dashboard',
+			  component: DashboardComponent
+			},
+			{
+			  path: '',
+			  redirectTo: '/dashboard',
+			  pathMatch: 'full'
+			},
+			{
+			  path: 'detail/:id',
+			  component: HeroDetailComponent
+			},
+
+		])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap: [ AppComponent ]
+  providers: [ 
+  	HeroService 
+  ],
+  bootstrap: [ AppComponent ],
 })
 export class AppModule { }
